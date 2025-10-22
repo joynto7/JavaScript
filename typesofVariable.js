27efdafe91406f542fd81084ca6a7b04c2b657c9
joynto7 <joyntoghosh7@gmail.com>
2025-10-22 15:49:18 +0600

    new type of variable

---
+++ typesofVariable.js
@@ -0,0 +1,11 @@
+let color = " red";
+console.log("color");
+console.log(typeof color);
+
+let age = 25 ;
+console.log("age");
+console.log(typeof age);
+
+let bird = "Parrot";
+console.log("bird");
+console.lof(typeof bird)
