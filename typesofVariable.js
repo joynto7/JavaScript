dd61d1d106ef636f0adda81d5a684f39b64806cc
joynto7 <joyntoghosh7@gmail.com>
2025-10-22 16:04:12 +0600

    new type of variable

--- typesofVariable.js
+++ typesofVariable.js
@@ -9,3 +9,12 @@ console.log(typeof age);
 let bird = "Parrot";
 console.log("bird");
 console.lof(typeof bird)
+
+const date = new Date("10-09 -1988");
+console.log(date);
+console.log(typeof date);
+ let carName = "BMW";
+ console.log("carName");
+ console.log(typeof carName);
+
+ 
