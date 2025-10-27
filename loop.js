777e24ffafcd85ddb225f929fb1e650f7080e128
joynto7 <joyntoghosh7@gmail.com>
2025-10-27 22:48:47 +0600

    Loops in Javascript

---
+++ loop.js
@@ -0,0 +1,10 @@
+function startCounting(){
+    console.log("Counting Started");
+
+    for(let i = 1; i <= 5; i++){
+        console.log("Current Count:"+i);
+
+    }
+    console.log("Counting Ended");
+
+}
