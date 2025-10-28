51e331c5287e00fbb816b0bf3260aa2bb0a81b88
joynto7 <joyntoghosh7@gmail.com>
2025-10-28 22:24:04 +0600

    New Object

--- object.js
+++ object.js
@@ -4,3 +4,13 @@ const car ={
     color :'black',
     year : 2025
 }
+
+// An object representing a flower
+const flower ={
+    type : 'rose',
+    color : 'black',
+    smell : 'Strong',
+    petals : 32
+}
+console.log(car);
+console.log(flower);
