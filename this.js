e0b4f548c8e1782e6c3815833c5a7f78fdb7d0c1
joynto7 <joyntoghosh7@gmail.com>
2025-10-28 22:42:58 +0600

    Using this keyoword

---
+++ this.js
@@ -0,0 +1,12 @@
+ // using this keyword in object
+ const person = {
+  firstName: "John",
+  lastName: "Doe",
+  id: 5566,
+  fullName : function() {
+    return this.firstName + " " + this.lastName;
+  },
+
+};
+console.log("Full Name:"+ person.fullName());
+
