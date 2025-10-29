60179f9a634036efb78b6c511c4bb39ccae5ed3a
joynto7 <joyntoghosh7@gmail.com>
2025-10-29 11:13:17 +0600

    string  destructuring

--- destructuring.js
+++ destructuring.js
@@ -9,3 +9,12 @@ const person = {
 const {firstname,lastname,age} = person;
 console.log(firstname +" "+ lastname);
 console.log(age)
+
+//string destructuring 
+
+const myString = "HelloWorld";
+const [w1,w2,w3,w4,w5,w6,w7,w8,w9,w10] = myString;
+console.log(w1 + w2 + w3 + w4 + w5);
+console.log(w6 + w7 + w8 + w9 + w10);
+
+
