7d896ff6faad66a5cef805e4a8b4af1ef006a0d4
joynto7 <joyntoghosh7@gmail.com>
2025-10-29 11:15:00 +0600

    array destructuring

--- destructuring.js
+++ destructuring.js
@@ -17,4 +17,11 @@ const [w1,w2,w3,w4,w5,w6,w7,w8,w9,w10] = myString;
 console.log(w1 + w2 + w3 + w4 + w5);
 console.log(w6 + w7 + w8 + w9 + w10);
 
+//// array destructuring 
+const colours =["red","green","blue"];
+const [color1,color2,color3]= colours;
+console.log(color1);
+console.log(color2);
+console.log(color3);
+
 
