e279db6f5a85c8c52bd840b08e9c486538b98787
joynto7 <joyntoghosh7@gmail.com>
2025-10-31 19:52:19 +0600

    creating array

---
+++ array.js
@@ -0,0 +1,19 @@
+const cars =[ "BMW","LAMBO","BUGGATII"];
+
+console.log(cars)
+
+const colors = [
+    "Black",
+    "Blue",
+    "White",
+    "Gray"
+];
+console.log(colors)
+
+const flowers = [
+    "Lily",
+    "Rose",
+    "Lotus",
+];
+console.log(flowers)
+console.log(flowers[0])
