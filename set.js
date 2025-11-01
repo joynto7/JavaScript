0e7c9253d048b8439b8fb55ffcb1d3a9ca9bb8ef
joynto7 <joyntoghosh7@gmail.com>
2025-11-01 21:58:16 +0600

    using add() & delete() to the set

--- set.js
+++ set.js
@@ -2,3 +2,10 @@
 
 const numbers = new Set ([1,2,3,5,65,7,]);
 console.log(numbers)
+
+//Add Values to the set
+numbers.add(23);
+numbers.add(67);
+
+// Delete values from the set
+numbers.delete(5);
