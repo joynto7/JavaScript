fd78cb40d49dd159a2447d71b5ee3be7fd2bb381
joynto7 <joyntoghosh7@gmail.com>
2025-11-01 22:14:04 +0600

    Using Add Method

--- setMethods.js
+++ setMethods.js
@@ -1,4 +1,12 @@
 // create a new set
 const cities = new Set(["Dhaka","chittaganj","Cumilla","Rajshahi"]);
 console.log((cities));
+// Using Add method
+cities.add("Mainkganj");
+cities.add("Cox's BaZar");
+cities.add("Rajshahi");
+cities.add("Bandarban");
+cities.add("Jessore");
+console.log(cities)
+
 
