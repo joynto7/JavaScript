65fdd78d3e9a253705d81ccaecf5a474498f3db2
joynto7 <joyntoghosh7@gmail.com>
2025-11-02 22:31:40 +0600

    Creating a jsoData

--- parse.js
+++ parse.js
@@ -1,4 +1,4 @@
-const jsonData = {
+const jsonData = `{
   "name": "Artificial",
   "age": 25,
   "location": "Dhaka, Bangladesh",
@@ -8,5 +8,15 @@ const jsonData = {
     "email": "artificial@example.com",
     "phone": "+880123456789"
   }
-};
+}`;
 
+
+//converting json into an object
+
+const obj = JSON.parse(jsonData);
+// show the output
+console.log(obj);
+console.log(obj.age);
+console.log(obj.name);
+console.log(obj.skills);
+console.log(obj.location);
