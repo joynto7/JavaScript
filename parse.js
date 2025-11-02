646e70b5db807a0df3c3b229b839723d083e93f0
joynto7 <joyntoghosh7@gmail.com>
2025-11-02 22:23:58 +0600

    Creating a jsoData

---
+++ parse.js
@@ -0,0 +1,12 @@
+const jsonData = {
+  "name": "Artificial",
+  "age": 25,
+  "location": "Dhaka, Bangladesh",
+  "skills": ["JavaScript", "HTML", "CSS"],
+  "isStudent": true,
+  "contact": {
+    "email": "artificial@example.com",
+    "phone": "+880123456789"
+  }
+};
+
