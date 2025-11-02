5542d0a89de64d2fc420c86d0495bfe8fed11c08
joynto7 <joyntoghosh7@gmail.com>
2025-11-02 21:55:59 +0600

    converting into a string

---
+++ json.js
@@ -0,0 +1,15 @@
+// Creating an new object
+
+const user = {
+  name: "alex jonathon",
+  age: 21,
+  skills: ["JavaScript", "Cybersecurity"]
+};
+
+// convert to JSON String
+
+const String = JSON.stringify(user);
+console.log(typeof(String))
+
+
+
