87ea46f573ff5e9e57400ff0c592b650236ff0ae
joynto7 <joyntoghosh7@gmail.com>
2025-11-05 21:47:00 +0600

    Profile Show

---
+++ ProblemSlove/user_profile.js
@@ -0,0 +1,9 @@
+let name = 'Alice';
+let age = 21;
+let isStudent = true;
+
+let studentStatus = isStudent? "YES" : "NO";
+
+let profile = `Name: ${name}, Age:${age}, Student :${studentStatus}`;
+
+console.log(profile);
