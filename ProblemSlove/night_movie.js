b679ecdee571ec9a1fb56691a085bbb46e773ac8
joynto7 <joyntoghosh7@gmail.com>
2025-11-05 21:56:03 +0600

    Night Movie Age

---
+++ ProblemSlove/night_movie.js
@@ -0,0 +1,7 @@
+let userAge = 25 ;
+
+if(userAge >= 18){
+    console.log("Welcome ! You are old enough to watch this movie");
+}else{
+    console.log("Sorry, You are not old enough to watch this movie");
+}
